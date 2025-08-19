fe14f6370c2979b55c159b8369de2fac05aaf637
Grady Zhuo <gradyzhuo@gmail.com>
2025-08-20 01:56:07 +0800

    [UPDATE] MuPDF

---
+++ Sources/MuPDF/Matrix.swift
@@ -0,0 +1,13 @@
+//
+//  Matrix.swift
+//  MuPDF
+//
+//  Created by Grady Zhuo on 2025/8/20.
+//
+
+import CMuPDF
+
+public struct Matrix: Sendable {
+    internal typealias UnderlyingType = fz_matrix
+    
+}
---
+++ Sources/MuPDF/Page.swift
@@ -0,0 +1,12 @@
+//
+//  Page.swift
+//  MuPDF
+//
+//  Created by Grady Zhuo on 2025/8/20.
+//
+import CMuPDF
+
+public struct Page: Sendable {
+    internal typealias UnderlyingType = fz_page
+    
+}
